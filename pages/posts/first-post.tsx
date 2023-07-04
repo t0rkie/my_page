576019141f217d929cea529c8e4d80c3d2b3d82e
Tokiya Akiyoshi <tokiya_akiyoshi@m3sol.co.jp>
2023-07-04 22:44:20 +0900

    fix_error

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,27 +1,21 @@
-// import type { FC } from 'react'
-// import Link from 'next/link'
-// import Image from 'next/image'
-// import Head from 'next/head'
-// import Layout from '@/components/Layout'
+import type { FC } from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 
-// const FirstPost: FC = () => {
-//   return (
-//     <Layout>
-//       <Head>
-//         <title>First Post</title>
-//       </Head>
-//       <h1>First Post</h1>
-//       <h2>
-//         <Link href="/">Back to home</Link>
-//       </h2>
-//       <Image
-//         src="/images/a.png"
-//         width={144}
-//         height={144}
-//         alt="profile"
-//       />
-//     </Layout>
-//   )
-// }
+const FirstPost: FC = () => {
+  return (
+    <div>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+      <Image
+        src="/images/a.png"
+        width={144}
+        height={144}
+        alt="profile"
+      />
+    </div>
+  )
+}
 
-// export default FirstPost
+export default FirstPost
