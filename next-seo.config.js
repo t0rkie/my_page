fe79952b6bd00ec45df67e94ccb435c0d6176c47
Tokiya Akiyoshi <tokiya_akiyoshi@m3sol.co.jp>
2023-07-14 18:20:08 +0900

    SEO対策ツール追加

---
+++ next-seo.config.js
@@ -0,0 +1,15 @@
+export default {
+  title: 'My Blog',
+  description: '技術情報を発信するブログです',
+  openGraph: {
+    type: 'website',
+    locale: 'ja_JP',
+    url: 'https://localhost:3000/',
+    site_name: 'My Page',
+  },
+  twitter: {
+    handle: '@user_name',
+    site: '@site',
+    cardType: 'summary_large_image'
+  }
+}
